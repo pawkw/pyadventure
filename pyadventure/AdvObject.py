57b84d2b6426e94dfbdedeb13199cc39d38606a8
pawkw <pawkw.users.noreply.github.com>
2021-08-01 16:41:47 -0400

    Started tests and AdvObject

--- pyadventure/AdvObject.py
+++ pyadventure/AdvObject.py
@@ -1,4 +1,13 @@
 
 
 class AdvObject:
-    pass
+    def __init__(self, description=None):
+        self._description = description
+
+    def set_description(self, description):
+        if description is None:
+            raise ValueError("AdvObject.set_description is None")
+        self._description = description
+
+    def get_description(self):
+        return self._description
--- tests/test_advobj.py
+++ tests/test_advobj.py
@@ -1,13 +1,26 @@
-import unittest
 import pyadventure.AdvObject as AdvObject
+
 
 class TestObject:
     greenBall = None
+    redBall = None
 
     @classmethod
     def setup_class(cls):
         cls.greenBall = AdvObject.AdvObject()
+        cls.redBall = AdvObject.AdvObject("red ball")
 
     def test_instantiate(self):
         assert self.greenBall is not None
 
+    def test_description(self):
+        self.greenBall.set_description("green ball")
+        assert self.greenBall.get_description() == "green ball"
+
+    # TODO: Test adding vocab
+
+    # TODO: Test match vocab
+
+    # TODO: Add child object and parent object
+
+    # TODO: Find child with vocab
