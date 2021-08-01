ea7aaee0b196e60a5e827a173eb91a0796eb2bb9
pawkw <pawkw.users.noreply.github.com>
2021-08-01 18:34:20 -0400

    Added parent-child relationship. Added find object with vocab.

--- pyadventure/AdvObject.py
+++ pyadventure/AdvObject.py
@@ -4,6 +4,8 @@ class AdvObject:
     def __init__(self, description=None):
         self._description = description
         self._vocab = set()
+        self._children = []
+        self._parent = None
 
     def set_description(self, description):
         if description is None:
@@ -19,3 +21,34 @@ class AdvObject:
 
     def get_vocab(self):
         return self._vocab
+
+    def match_vocab(self, *words):
+        test_vocab = set(words)
+        return self._vocab.intersection(test_vocab) == test_vocab
+
+    def add_child(self, child):
+        if child not in self._children:
+            self._children.append(child)
+        if child.get_parent() is not self:
+            child.set_parent(self)
+
+    def get_parent(self):
+        return self._parent
+
+    def set_parent(self, parent):
+        if self._parent is not parent:
+            self._parent = parent
+        if self._parent.has_child(self) is not True:
+            self._parent.add_child(self)
+
+    def has_child(self, child):
+        return child in self._children
+
+    def find(self, *words):
+        result = []
+        if self.match_vocab(*words):
+            result.append(self)
+        for child in self._children:
+            if child.match_vocab(*words):
+                result.append(child)
+        return result
--- tests/test_advobj.py
+++ tests/test_advobj.py
@@ -4,12 +4,13 @@ import pyadventure.AdvObject as AdvObject
 class TestObject:
     greenBall = None
     redBall = None
+    basket = None
 
     @classmethod
     def setup_class(cls):
         cls.greenBall = AdvObject.AdvObject()
         cls.redBall = AdvObject.AdvObject("red ball")
-        cls.redBall.add_vocab("red", "ball")
+        cls.basket = AdvObject.AdvObject("basket")
 
     def test_instantiate(self):
         assert self.greenBall is not None
@@ -26,5 +27,16 @@ class TestObject:
         assert self.greenBall.match_vocab("red", "ball") is False
 
     # Add child object and parent object
+    def test_parent_child(self):
+        self.basket.add_child(self.redBall)
+        assert(self.basket.has_child(self.redBall))
+        self.greenBall.set_parent(self.basket)
+        assert(self.basket.has_child(self.greenBall))
+        assert(self.greenBall.get_parent() is self.basket)
 
     # Find child with vocab
+    def test_find(self):
+        self.redBall.add_vocab("red", "ball")
+        self.basket.add_child(self.redBall)
+        self.basket.add_child(self.greenBall)
+        assert(self.basket.find("ball") == [self.redBall, self.greenBall])
